86b3864ebe4d885a3a42f38a4bd51fa7052909de
lavingtaine <94486835+lavingtaine@users.noreply.github.com>
2021-12-13 18:11:50 +0100

    Create plateforme.py

---
+++ plateforme.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Plateforme(pygame.sprite.Sprite):
+
+    def __init__(self, rect ):
+        super().__init__()
+        self.rect = rect
+
+    def afficher (self, surface):
+       pygame.draw.rect(surface, ( 0, 255, 0), self.rect)
