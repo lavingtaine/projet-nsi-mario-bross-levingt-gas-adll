3bb1c70635aed0f27fcf3950180ad228eca583f4
lavingtaine <94486835+lavingtaine@users.noreply.github.com>
2021-12-29 19:20:35 +0100

    Create projectiles.py

---
+++ projectiles.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Projectile(pygame.sprite.Sprite):
+
+    def __init__ (self, x , y , taille, direction , image):
+
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.taille = taille
+        self.direction = direction
+        self.image = image
+        self.rect = pygame.Rect (self.x, self.y, self.taille[0], self.taille[1])
+
+
+    def afficher( self, surface, delta_temps):
+       if delta_temps > 2:
+          self.image = pygame.transform.scale(self.image, (30,30))
+          surface.blit(self.image,self.rect)
+
+    def mouvement(self, vitesse):
+        self.rect.x += ( vitesse *  self.direction)
+
+
+class Slash(pygame.sprite.Sprite):
+    def __init__(self, x , y , taille, image):
+
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.taille = taille
+        self.image = image
+        self.rect = pygame.Rect (self.x, self.y, self.taille[0], self.taille[1])
+
+    def afficher(self, surface):
+
+
+
+        surface.blit(self.image, self.rect)
+
+    def mouvement(self, vitesse):
+        self.rect.x += vitesse
